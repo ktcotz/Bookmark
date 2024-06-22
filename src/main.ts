a6a7e178ce39d52b278135ed55a343c1343d95c5
ktcotz <naskret.kamil@gmail.com>
2024-06-22 14:06:20 +0200

    feat: cta form
    Work around cta + cta-form, styling and state's.
    Work with errors and responsivity.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import "./style/style.scss";
 import { Faq } from "./ts/faq";
 import { Features } from "./ts/features";
+import { Form } from "./ts/form";
 import { Navigation } from "./ts/navigation";
 
 const init = () => {
   new Navigation().initialize();
   new Features().initialize();
   new Faq().initialize();
+  new Form().initialize();
 };
 
 init();
---
+++ src/ts/form.ts
@@ -0,0 +1,5 @@
+export class Form {
+  initialize() {
+    console.log("form");
+  }
+}
